8ef980290a7b2f9c4401fe9df43276b2073bb1a3
Kaung Zaw Thant <kaung.thant@aiesec.net>
2024-07-30 15:16:26 -0500

    fixed base url for daily function

--- weather_app/core/views.py
+++ weather_app/core/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 import requests
 import json
-
+from datetime import datetime
 def get_weather_data(city):
-  
     Api_key = "d58c475f4f008441996e9bb1a2343ed3"
     base_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={Api_key}"
     parameters = {
         'q' : city,
         'appid' : Api_key,
+        'exclude' : 'minutely,hourly',
         'units' : 'metric',
     }
     try:
@@ -25,16 +25,8 @@ def get_weather_data(city):
 def index(request):
     city = request.GET.get('city')
     icon_url = 'https://openweathermap.org/img/wn/10d@2x.png'
-    weather = None
-    weather_description = None
-    country = None
-    wind_speed = None
-    pressure = None
-    humidity = None
-    temperature = None
     if city:
         weather_data_result = get_weather_data(city)
-    
         if weather_data_result is not None:
             weather_data = json.dumps(weather_data_result,indent=4)
             print(weather_data)
@@ -65,5 +57,52 @@ def index(request):
         'humidity': humidity,
         'temperature': temperature,
     })
+def daily(city):
+    #Api_key = "d58c475f4f008441996e9bb1a2343ed3"
+    cnt = 7
+    Api_key = "2b460c2135a2d363006e86ad30592fdf"
+    base_url = f"https://api.openweathermap.org/data/2.5/forecast"
+    parameters = {
+        'q' : city,
+        'appid' : Api_key,
+        'units' : 'metric',
+    }
+    try:
+        response = requests.get(base_url,params=parameters)
+        if response.status_code == 200:
+            return response.json()
+        else: 
+            return None
+    except requests.exceptions.RequestException as e:
+        response = None
 
+def detail(request,city):
+    forecast = daily(city)
+    weather_data_result = get_weather_data(city)
     
+    if weather_data_result:
+        city = weather_data_result['name']
+        weather = weather_data_result['weather'][0]['main']
+        temperature = weather_data_result['main']['temp']
+        forecast_data = []
+        for day in forecast['list']:
+            date = datetime.fromtimestamp(day['dt']).strftime('%A')
+            icon_id = day['weather'][0]['icon']
+            icon_url = f"https://openweathermap.org/img/wn/{icon_id}@2x.png"
+            day_weather = {
+                'date': date,
+                'icon': icon_url,
+                'temp_min': day['main']['temp_min'],
+                'temp_max': day['main']['temp_max']
+            }
+            forecast_data.append(day_weather)
+    else:
+        return render(request, 'core/detail.html')
+    return render(request,'core/detail.html', {
+        'city':city,
+        'weather':weather,
+        'temperature':temperature,
+        'forecast':forecast_data,
+        })
+
+
